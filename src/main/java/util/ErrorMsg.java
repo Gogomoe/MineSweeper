626c38b535ef619235152bab78c76ef49fed829e
Gogo <gogo81745.moe@gmail.com>
2021-08-22 21:26:53 +0800

    refactor: migrate to gradle

---
+++ src/main/java/util/ErrorMsg.java
@@ -0,0 +1,16 @@
+/**
+ * @Author: RogerDTZ
+ * @FileName: ErrorMsg.java
+ */
+
+package util;
+
+import javax.swing.*;
+
+public class ErrorMsg {
+
+    public static void error(String msg) {
+        JOptionPane.showMessageDialog(null, msg, "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+}
